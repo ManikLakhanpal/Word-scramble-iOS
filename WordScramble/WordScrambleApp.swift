440def2b411cb1637c3e1f3307af0f050268f9e0
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2024-06-22 16:08:19 +0530

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Manik Lakhanpal on 22/06/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
